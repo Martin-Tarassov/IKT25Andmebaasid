819f90a28335b5c42ed408839bb6a0939c0fe03b
Martin Tarassov <epicgamesmega1@gmail.com>
2026-03-04 11:49:10 +0200

    Add files via upload

---
+++ IKT25/IKT25db.sql
@@ -0,0 +1,435 @@
+--17.02.2026
+--tund nr 1
+
+-- teeme andmebaasi e db
+create database IKT25tar
+
+--andmebaasi valimine
+use IKT25tar
+
+--andmebaasi kustutamine koodiga
+--otsida kood ülesse
+drop database IKT25tar
+
+--teeme uuesti andmebaasi IKT25tar
+create database IKT25tar
+
+--teeme tabeli
+create table Gender
+(
+--Meil on muutuja Id,
+--mis on täisarv andmetüüp,
+--kui sisestad andmed, 
+--siis see veerg peab olema täidetud,
+--tegemist on primaarvõtmega
+Id int not null primary key,
+--veeru nimi on Gender,
+--10 tähemärki on max pikkus,
+--andmed peavad olema sisestatud e 
+--ei tohi olla tühi
+Gender nvarchar(10) not null
+)
+
+--andmete sisestamine Gender tabelisse
+--proovige ise teha
+-- Id = 1, Gender = Male
+-- Id = 2, Gender = Female
+insert into Gender (Id, Gender)
+values (1, 'Male'),
+(2, 'Female')
+
+--vaatame tabeli sisu
+-- * tähendab, et näita kõike seal sees olevat infot
+select * from Gender
+
+--teeme tabeli nimega Person
+--veeru nimed: Id int not null primary key,
+-- Name nvarchar (30)
+-- Email nvarchar (30)
+--GenderId int
+create table Person
+(
+Id int not null primary key,
+Name nvarchar(30),
+Email nvarchar(30),
+GenderId int
+)
+
+--18.02.2026
+--tund nr 2
+
+insert into Person (Id, Name, Email, GenderId)
+values (1, 'Superman', 's@s.com', 2),
+(2, 'Wonderwoman', 'w@w.com', 1),
+(3, 'Batman', 'b@b.com', 2),
+(4, 'Aquaman', 'a@a.com', 2),
+(5, 'Catwoman', 'c@c.com', 1),
+(6, 'Antman', 'ant"ant.com', 2),
+(8, NULL, NULL, 2)
+
+--näen tabelis olevat infot
+select * from Person
+
+--võõrvõtme ühenduse loomine kahe tabeli vahel
+alter table Person add constraint tblPerson_GenderId_FK
+foreign key (GenderId) references Gender(Id)
+
+-- kui sisestad uue rea andmeid ja ei ole sisestanud GenderId alla 
+-- väärtust, siis automaatselt sisestab sellele reale väärtuse 3
+-- e unknown
+alter table Person
+add constraint DF_Persons_GenderId
+default 3 for GenderId
+
+insert into Gender (Id, Gender)
+values (3, 'Unknown')
+
+insert into Person (Id, Name, Email, GenderId)
+values (7, 'Black Panther', 'b@b.com', NULL)
+
+insert into Person (Id, Name, Email)
+values (9, 'Spiderman', 'spider@man.com')
+
+select * from Person
+
+--piirnagu kustutamine
+alter table Person
+drop constraint DF_Persons_GenderId
+
+--kuidas lisada veergu tabelile Person
+--veeru nimi on Age nvarchar(10)
+alter table Person
+add Age nvarchar(10)
+
+alter table Person
+add constraint CK_Person_Age check (Age > 0 and Age < 155)
+
+-- kuidas uuendada andemeid
+update Person
+set Age = 159
+where Id = 7
+
+select * from Person
+
+--soovin kustutada ühe rea
+-- kuidas seda teha????
+delete from Person where Id = 8
+
+select * from Person
+
+--lisame uue veeru City nvarchar(50)
+alter table Person
+add City nvarchar(50)
+
+--kõik, kes elavad Gothami linnas
+select * from Person where City = 'Gotham'
+-- k]ik, kes ei ela Gothamis
+select * from Person where City != 'Gotham'
+-- variant nr 2. K]ik, kes ei ela Gothamis
+select * from Person where City <> 'Gotham'
+
+--näitab teatud vanusega inimesi
+-- valime 151, 35, 26
+select * from Person where Age in (151, 35, 26)
+select * from Person where Age = 151 or Age = 35 or Age = 26
+
+-- soovin näha inimesi vahemikus 22 kuni 41
+select * from Person where Age between 22 and 41
+
+--wildcard e näitab kõik g-tähega linnad
+select * from Person where City like 'g%'
+--otsib emailid @-märgiga
+select * from Person where Email like '%@%'
+
+--tahan näha, kellel on emailis ees ja peale @-märki üks täht
+select * from Person where Email like '_@_.com'
+
+--kõik, kelle nimes ei ole esimene täht W, A, S
+select * from Person where Name like '[^WAS]%'
+
+--k]ik, kes elavad Gothamis ja New Yorkis
+select * from Person where (City = 'Gotham' or City = 'New York')
+
+-- k]ik, kes elavad Gothamis ja New Yorkis ning peavad olema 
+-- vanemad, kui 29
+select * from Person where (City = 'Gotham' or City = 'New York')
+and Age >= 30
+
+--kuvab tähestikulises järjekorras inimesi ja võtab aluseks 
+-- Name veeru
+select * from Person
+select * from Person order by Name
+
+--võtab kolm esimest rida Person tabelist
+select top 3 * from Person
+
+--tund 3
+--25.02.2026
+--kolm esimest, aga tabeli järjestus on Age ja siis Name
+select top 3 Age, Name from Person
+
+--näita esimesed 50% tabelist
+select top 50 percent * from Person
+select * from Person
+
+--järjestab vanuse järgi isikud
+select * from Person order by Age desc
+
+--muudab Age muutuja int-ks ja näitab vanuselises järjestuses
+-- cast abil saab andmetüüpi muuta
+select * from Person order by cast(Age as int) desc
+
+-- kõikide isikute koondvanus e liidab kõik kokku
+select sum(cast(Age as int)) from Person
+
+--kõige noorem isik tuleb üles leida
+select min(cast(Age as int)) from Person
+
+--kõige vanem isik
+select max(cast(Age as int)) from Person
+
+--muudame Age muutuja int peale
+-- näeme konkreetsetes linnades olevate isikute koondvanust
+select City, sum(Age) as TotalAge from Person group by City
+
+--kuidas saab koodiga muuta andmetüüpi ja selle pikkust
+alter table Person 
+alter column Name nvarchar(25)
+
+-- kuvab esimeses reas välja toodud järjestuses ja kuvab Age-i 
+-- TotalAge-ks
+--järjestab City-s olevate nimede järgi ja siis Genderid järgi
+--kasutada group by-d ja order by-d
+select City, GenderId, sum(Age) as TotalAge from Person
+group by City, GenderId
+order by City
+
+--näitab, et mitu rida andmeid on selles tabelis
+select count(*) from Person
+
+--näitab tulemust, et mitu inimest on Genderid väärtusega 2
+--konkreetses linnas
+--arvutab vanuse kokku selles linnas
+select GenderId, City, sum(Age) as TotalAge, count(Id) as 
+[Total Person(s)] from Person
+where GenderId = '1'
+group by GenderId, City
+
+--näitab ära inimeste koondvanuse, mis on üle 41 a ja
+--kui palju neid igas linnas elab
+--eristab inimese soo ära
+select GenderId, City, sum(Age) as TotalAge, count(Id) as 
+[Total Person(s)] from Person
+where GenderId = '2'
+group by GenderId, City having sum(Age) > 41
+
+--loome tabelid Employees ja Department
+create table Department
+(
+Id int primary key,
+DepartmentName nvarchar(50),
+Location nvarchar(50),
+DepartmentHead nvarchar(50)
+)
+
+create table Employees
+(
+Id int primary key,
+Name nvarchar(50),
+Gender nvarchar(50),
+Salary nvarchar(50),
+DepartmentId int
+)
+
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (1, 'Tom', 'Male', 4000, 1),
+(2, 'Pam', 'Female', 3000, 3),
+(3, 'John', 'Male', 3500, 1),
+(4, 'Sam', 'Male', 4500, 2),
+(5, 'Todd', 'Male', 2800, 2),
+(6, 'Ben', 'Male', 7000, 1),
+(7, 'Sara', 'Female', 4800, 3),
+(8, 'Valarie', 'Female', 5500, 1),
+(9, 'James', 'Male', 6500, NULL),
+(10, 'Russell', 'Male', 8800, NULL)
+
+insert into Department(Id, DepartmentName, Location, DepartmentHead)
+values 
+(1, 'IT', 'London', 'Rick'),
+(2, 'Payroll', 'Delhi', 'Ron'),
+(3, 'HR', 'New York', 'Christie'),
+(4, 'Other Department', 'Sydney', 'Cindrella')
+
+select * from Department
+select * from Employees
+
+---
+select Name, Gender, Salary, DepartmentName
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+---
+
+--arvutab k]ikide palgad kokku Employees tabelist
+select sum(cast(Salary as int)) from Employees --arvutab kõikide palgad kokku
+-- kõige väiksema palga saaja
+select min(cast(Salary as int)) from Employees
+
+--näitab veerge Location ja Palka. Palga veerg kuvatakse TotalSalary-ks
+--teha left join Department tabeliga
+--grupitab Locationiga
+select Location, sum(cast(Salary as int)) as TotalSalary
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+group by Location
+
+
+-- 5 tund
+--
+
+--kustutame veeru nimega City Employee tabelist
+alter table Employees
+drop column City
+
+--inner join 
+--kuvab neid, kellel on DepartmentName all olemas väärtus
+--mitte kattuvad read eemaldatakse tulemusest
+--ja sellepärast ei näidata Jamesi ja Russelit tabelis
+--kuna neil on DepartmentId NULL
+select Name, Gender, Salary, DepartmentName
+from Employees
+inner join Department
+on Employees.DepartmentId = Department.Id
+
+--left join 
+select Name, Gender, Salary, DepartmentName 
+from Employees 
+left join Department --võib kasutada ka LEFT OUTER JOIN-i
+on Employees.DepartmentId = Department.Id
+--uuriga,mis on left join
+--James ja Russell on Employees tabelis, aga neil pole DepartmentId,
+--LEFT JOIN näitab neid ikkagi, ainult DepartmentName on tühi
+--näitab andmeid, kus vasakpoolsest tabelist isegi, siis seal puudub
+--võõrvõtme reas väärtus
+
+--right join
+select Name, Gender, Salary, DepartmentName
+from Employees 
+right join Department 
+on Employees.DepartmentId = Department.Id
+--right join näitab paremas tabelis olevaid väärtuseid,
+--mis ei ühti vasaku (Employees) tabeliga 
+
+--outer join
+select Name, Gender, Salary, DepartmentName 
+from Employees full 
+outer join Department 
+on Employees.DepartmentId = Department.Id
+--mõlema tabeli read kuvab
+
+--teha cross join
+select Name, Gender, Salary, DepartmentName
+from Employees
+cross join Department
+--korrutab kõik omavahel läbi
+
+--teha inner join, kus Employees tabelist DepartmentId on null
+select Name, Gender, Salary, DepartmentName
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+where Employees.DepartmentId is null
+
+--teine variant
+select Name, Gender, Salary, DepartmentName
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+where Department.Id is null
+--näitab ainult neid, kellel on vasakus tabelis (Employees)
+--DepartmentId null
+
+select Name, Gender, Salary, DepartmentName
+from Employees
+right join Department
+on Employees.DepartmentId = Department.Id
+where Employees.DepartmentId is null
+--näitab ainult paremas tabelis olevat rida,
+--mis ei kattu Employees-ga.
+
+--full join 
+--mõlema tabeli mitte-kattuvate väärtusega read kuvab välja
+
+select Name, Gender, Salary, DepartmentName
+from Employees
+full join Department
+on Employees.DepartmentId = Department.Id
+where Employees.DepartmentId is null
+or Department.Id is null;
+
+--teete AdventureWorksLT2019 andmebaasile join p ringuid:
+--inner join, left join, right join, crossjoin ja full join
+--tabeleid sellesse andmebaasi juurde ei tohi teha
+
+--inner join(1)
+select saleslt.customer.customerid,
+saleslt.customer.firstname,
+saleslt.customer.lastname,
+saleslt.salesorderheader.salesorderid,
+saleslt.salesorderheader.totaldue
+from saleslt.customer
+inner join saleslt.salesorderheader
+on saleslt.customer.customerid = saleslt.salesorderheader.customerid;
+
+--inner join(2)
+--mõnikord peab muutuja ette kirjutama tabeli nimetuse nagu on Product.Name,
+--et editor saaks aru, et kumma tabeli mutujat soovitakse kkasutada jaei tekiks
+--segadust 
+select Product.Name as [Product Name], ProductNumber, ListPrice,
+ProductModel.Name as [Product Model Name], 
+PRODUCT.ProductModelId, ProductModel.ProductModelId
+--mõnikotd peab ka tabeli ette kirjutama täpsustama info
+--nagu on SaleLt.Product
+from SalesLT.Product
+inner join SalesLT.ProductModel
+--antud juhul Producti tabelis ProdctModelId vöörvõti,
+--mis ProductModeli tabelis on primaarvõti
+on Product.ProductModelId = ProductModel.ProductModelId
+
+--left join
+select saleslt.customer.customerid,
+saleslt.customer.firstname,
+saleslt.customer.lastname,
+saleslt.salesorderheader.salesorderid
+from saleslt.customer
+left join saleslt.salesorderheader
+on saleslt.customer.customerid = saleslt.salesorderheader.customerid;
+
+
+--right join
+select saleslt.customer.customerid,
+saleslt.customer.firstname,
+saleslt.salesorderheader.salesorderid
+from saleslt.customer
+right join saleslt.salesorderheader
+on saleslt.customer.customerid = saleslt.salesorderheader.customerid;
+
+
+--full join
+select saleslt.customer.customerid,
+saleslt.customer.firstname,
+saleslt.salesorderheader.salesorderid
+from saleslt.customer
+full join saleslt.salesorderheader
+on saleslt.customer.customerid = saleslt.salesorderheader.customerid;
+
+--cross join
+select saleslt.customer.customerid,
+saleslt.customer.firstname,
+saleslt.productcategory.productcategoryid,
+saleslt.productcategory.name as categoryname
+from saleslt.customer
+cross join saleslt.productcategory;
+
+
